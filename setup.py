91343e7740a531958b5b4d1c80c7be6b28484c50
Cam <cam@duo.com>
2017-02-12 20:43:13 -0500

    Add setup.py to module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='twilio',
+    version='0.1',
+    description='Twilio helper module',
+    url='',
+    author='Cam Herringshaw',
+    author_email='camherringshaw@gmail.com',
+    license='MIT',
+    packages=['twilio'],
+    zip_safe=False,
+)
